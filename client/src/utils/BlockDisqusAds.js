b05109e42c39b46c3283fa492909a962bfae2b0e
Saheel Sapovadia <saheelsapovadia23@gmail.com>
2023-05-18 11:25:01 +0530

    check

--- client/src/utils/BlockDisqusAds.js
+++ client/src/utils/BlockDisqusAds.js
@@ -1,8 +1,8 @@
 const BlockDisqusAds = () => {
   setTimeout(() => {
-    var dDiv = document.getElementById("disqus_thread");
-    var iframe = dDiv.getElementsByTagName("iframe")[0];
-    dDiv.removeChild(dDiv.getElementsByTagName("iframe")[0]);
+    // var dDiv = document.getElementById("disqus_thread");
+    // var iframe = dDiv.getElementsByTagName("iframe")[0];
+    // dDiv.removeChild(dDiv.getElementsByTagName("iframe")[0]);
   }, 5500);
 };
 
